87deee3031bbc4f5664cc7551415b5a3dd8b0994
Tri Indranita <abdul.aziz@photoninfotech.net>
2017-08-10 12:50:34 +0700

    Support cors

--- src/main/java/com/manulife/claim/controller/ClaimController.java
+++ src/main/java/com/manulife/claim/controller/ClaimController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.manulife.claim.model.Claim;
 import com.manulife.claim.service.ClaimService;
 import com.manulife.claim.util.CustomErrorType;
 
+@CrossOrigin("http://manulife-claim-dockermgmt.centralus.cloudapp.azure.com")
 @RestController
 public class ClaimController {
 	
