094e8900dbc2c82fc53e29636f498bf4ffddb036
Aryo Kusumo <aryo.kusumo@photoninfotech.net>
2017-10-06 22:55:55 +0700

    change buildservice.sh images tag name from 2.0 to 1.0

--- buildservice.sh
+++ buildservice.sh
@@ -6,10 +6,10 @@ docker logout
 docker login -u photondevops -p Welcome123 -e ratihnurmalasari01@gmail.com
 
 # Build docker images
-docker build -t photondevops/photon-devops:springboot-service.2.0 .
+docker build -t photondevops/photon-devops:springboot-service.1.0 .
 
 # Push docker images to docker repo
-docker push photondevops/photon-devops:springboot-service.2.0
+docker push photondevops/photon-devops:springboot-service.1.0
 
 docker logout
 
